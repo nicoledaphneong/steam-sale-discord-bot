fc064dae3a1f66f4e837ee33808c5681fe2c0253
Nicole Daphne C. Ong <nicole_daphne_ong@dlsu.edu.ph>
2025-12-28 22:03:44 +0800

    Add price and lookup for name to appid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
+import re
 
+region = "PH"
 gamesList = []
 game = ""
 
@@ -11,13 +13,27 @@ while game != "EXIT":
 
 # Loop the list of games, and fetch the game name and discount from SteamSpy API
 for i in range(len(gamesList)):
+
+    # Use Steam store suggest search to get the app ID
+    searchGame = requests.get("https://store.steampowered.com/search/suggest",
+        params={"term": gamesList[i], "f": "games", "cc": region, "l": "en"}
+    )
+    searchGame = searchGame.text
+    print(searchGame)
+    appId = re.findall(r'data-ds-appid="(\d+)"', searchGame)
+    price = re.findall(r'match_price">\s*([^<]+)', searchGame)
+    price = float(re.sub(r"[^\d.]", "", price[0]))
+
     data = requests.get(
         "https://steamspy.com/api.php",
-        params={"request": "appdetails", "appid": gamesList[i]},
+        params={"request": "appdetails", "appid": appId[0]},
     ).json()
 
     name = data["name"]
     discount = data["discount"]
+    original = price / (1 - int(discount)/100)
 
     print("Name:", name)
     print("Discount:", discount, "%")
+    print("Price: ", price)
+    print("Original Price: ", original)
