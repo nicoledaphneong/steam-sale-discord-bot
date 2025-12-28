45f98e1d00e0b760990596f9d1618166a9ad59c8
Nicole Daphne C. Ong <nicole_daphne_ong@dlsu.edu.ph>
2025-12-28 17:35:19 +0800

    Add loop to take user input

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import requests
 
-data = requests.get(
-    "https://steamspy.com/api.php",
-    params={"request": "appdetails", "appid": 413150}
-).json()
+gamesList = []
+game = ""
 
-name = data["name"]
-discount = data["discount"]
+# Get game id user input, until user types "EXIT"
+while game != "EXIT":
+    game = input("Enter the name of a game (or type 'EXIT' to quit): ")
+    if game != "EXIT":
+        gamesList.append(game)
 
-print("Name:", name)
-print("Discount:", discount, "%")
+# Loop the list of games, and fetch the game name and discount from SteamSpy API
+for i in range(len(gamesList)):
+    data = requests.get(
+        "https://steamspy.com/api.php",
+        params={"request": "appdetails", "appid": gamesList[i]},
+    ).json()
+
+    name = data["name"]
+    discount = data["discount"]
+
+    print("Name:", name)
+    print("Discount:", discount, "%")
